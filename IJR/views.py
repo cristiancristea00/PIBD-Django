9876d4c1fc6f8bca430bec2d2f865fb913bb934d
Cristian Cristea <cristiancristea00@gmail.com>
2021-12-26 19:49:21 +0200

    CRUD functionality on Judecator

--- IJR/views.py
+++ IJR/views.py
@@ -1,4 +1,7 @@
+from dateutil.parser import parse
 from django.views.generic import TemplateView
+
+from IJR.models import *
 
 
 # Create your views here.
@@ -8,9 +11,6 @@ class HomePageView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
-        from IJR.models import Proces
-        from IJR.models import Judecator
-
         context = super().get_context_data(**kwargs)
         procese = Proces.objects.all()
         judecatori = Judecator.objects.all()
@@ -18,25 +18,62 @@ class HomePageView(TemplateView):
         context['judecatori'] = judecatori
         return context
 
+    def post(self, request, *args, **kwargs):
+        if self.request.POST.get('addJudecator') is not None:
+            judecator = Judecator()
+            judecator.cnp = self.request.POST.get('CNP_add', None)
+            judecator.nume = self.request.POST.get('Nume_add', None)
+            judecator.prenume = self.request.POST.get('Prenume_add', None)
+            judecator.telefon = self.request.POST.get('Telefon_add', None)
+            judecator.email = self.request.POST.get('Email_add', None)
+            judecator.specializare = self.request.POST.get('Specializare_add', None)
+            judecator.preluare_mandat = parse(self.request.POST.get('Preluare_mandat_add', None))
+            judecator.expirare_mandat = parse(self.request.POST.get('Expirare_mandat_add', None))
+            judecator.save(force_insert=True)
+        return self.render_to_response(self.get_context_data())
+
 
 class JudecatoriPageView(TemplateView):
     template_name = 'judecatori.html'
 
     def get_context_data(self, **kwargs):
-        from IJR.models import Judecator
-
         context = super().get_context_data(**kwargs)
         judecatori = Judecator.objects.all()
         context['judecatori'] = judecatori
         return context
+
+    def post(self, request, *args, **kwargs):
+        if self.request.POST.get('updateJudecator') is not None:
+            judecator = Judecator.objects.get(id_judecator=int(self.request.POST.get('Select_judecator_Update', None)))
+            cnp = self.request.POST.get('CNP_update', None)
+            cnp = cnp if cnp != '' else judecator.cnp
+            nume = self.request.POST.get('Nume_update', None)
+            nume = nume if nume != '' else judecator.nume
+            prenume = self.request.POST.get('Prenume_update', None)
+            prenume = prenume if prenume != '' else judecator.prenume
+            telefon = self.request.POST.get('Telefon_update', None)
+            telefon = telefon if telefon != '' else judecator.telefon
+            email = self.request.POST.get('Email_update', None)
+            email = email if email != '' else judecator.email
+            specializare = self.request.POST.get('Specializare_update', None)
+            specializare = specializare if specializare != '' else judecator.specializare
+            preluare_mandat = self.request.POST.get('Preluare_mandat_update', None)
+            preluare_mandat = parse(preluare_mandat) if preluare_mandat != '' else judecator.preluare_mandat
+            expirare_mandat = self.request.POST.get('Expirare_mandat_update', None)
+            expirare_mandat = parse(expirare_mandat) if expirare_mandat != '' else judecator.expirare_mandat
+            judecator = Judecator(id_judecator=judecator.id_judecator, cnp=cnp, nume=nume, prenume=prenume, telefon=telefon, email=email,
+                                  specializare=specializare, preluare_mandat=preluare_mandat, expirare_mandat=expirare_mandat)
+            judecator.save(force_update=True)
+        elif self.request.POST.get('deleteJudecator') is not None:
+            judecator = Judecator.objects.get(id_judecator=int(self.request.POST.get('Select_judecator_Delete', None)))
+            judecator.delete()
+        return self.render_to_response(self.get_context_data())
 
 
 class ProcesePageView(TemplateView):
     template_name = 'procese.html'
 
     def get_context_data(self, **kwargs):
-        from IJR.models import Proces
-
         context = super().get_context_data(**kwargs)
         procese = Proces.objects.all()
         context['procese'] = procese
@@ -47,8 +84,6 @@ class ProgramariPageView(TemplateView):
     template_name = 'programari.html'
 
     def get_context_data(self, **kwargs):
-        from IJR.models import Programare
-
         context = super().get_context_data(**kwargs)
         programari = Programare.objects.all()
         context['programari'] = programari
